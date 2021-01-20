a3cdb25da9f222ecb00cdec936af1b9c7a7bfb9c
itpro-tips <44789311+itpro-tips@users.noreply.github.com>
2021-01-20 18:04:49 +0100

    Create Get-DNSOrphanedZones.ps1

---
+++ DNS/Get-DNSOrphanedZones.ps1
@@ -0,0 +1,18 @@
+function Get-DNSOrphanedZones {
+    param(
+        # Parameter help description
+        [Parameter(Mandatory)]
+        [String]$DomainNamingContext
+    )
+    $DNSOrphanedZones = New-Object System.Collections.ArrayList
+
+    Get-ADObject -SearchBase "CN=MicrosoftDNS,DC=DomainDnsZones,$DomainNamingContext" -SearchScope OneLevel -Filter { Name -like '*CNF:*' } | ForEach-Object {
+        $null = $DNSOrphanedZones.Add($_.DistinguishedName)
+    }
+    
+    Get-ADObject -SearchBase "CN=MicrosoftDNS,DC=ForestDnsZones,$DomainNamingContext" -SearchScope OneLevel -Filter { Name -like '*CNF:*' } | ForEach-Object {
+        $null = $DNSOrphanedZones.Add($_.DistinguishedName)
+    }
+    
+    return $DNSOrphanedZones
+}
