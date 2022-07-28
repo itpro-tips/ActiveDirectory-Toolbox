62128b8559520c477d2bc62f4c43e76c09cdf75e
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2022-07-28 12:37:56 +0200

    Create Get-DLLLoadedByProcesses

---
+++ System/Get-DLLLoadedByProcesses.ps1
@@ -0,0 +1,48 @@
+function Get-DLLLoadedByProcesses {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [String]$ComputerName,
+        [Parameter(Mandatory = $false)]
+        [String[]]$ProcessName
+    )
+    
+    [System.Collections.Generic.List[PSObject]]$dllArray = @()
+
+    if($ProcessName){
+        if ($computerName) {
+            $allProcesses = Get-Process -ComputerName $ComputerName -ProcessName $ProcessName
+        }
+        else {
+            $allProcesses = Get-Process -ProcessName $ProcessName
+        }
+    }
+    else {
+        if ($computerName) {
+            $allProcesses = Get-Process -ComputerName $ComputerName
+        }
+        else {
+            $allProcesses = Get-Process
+        }
+    }    
+
+    foreach ($process in $allProcesses) {
+        foreach ($module in $process.modules) {
+
+            $object = [PSCustomObject][ordered]@{
+                Process              = $process.Name
+                ProcessID            = $process.ID
+                ModuleName           = $module.ModuleName
+                ModuleFileName       = $module.FileName
+                ModuleSize           = $module.Size
+                ModuleDescription    = $module.Description
+                ModuleProduct        = $module.Product
+                ModuleProductVersion = $module.ProductVersion
+            }
+
+            $dllArray.Add($object)
+        }
+    }
+
+    return $dllArray
+}
