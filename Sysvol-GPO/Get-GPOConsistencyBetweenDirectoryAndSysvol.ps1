7f3e826d224480d007b6bbf2e2b5a84efc057bac
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2022-09-28 16:05:27 +0200

    Update Get-GPOConsistencyBetweenDirectoryAndSysvol.ps1

--- Sysvol-GPO/Get-GPOConsistencyBetweenDirectoryAndSysvol.ps1
+++ Sysvol-GPO/Get-GPOConsistencyBetweenDirectoryAndSysvol.ps1
@@ -1,5 +1,5 @@
 ﻿# Get GPO in Active Directory and Sysvol and compare if present
-function Get-GPOConsistencyBetweenDirectoryAndSysvol {
+function Get-GPOConsistencyArrayBetweenDirectoryAndSysvol {
     Param(
         [Parameter(Mandatory = $false)]
         [string[]]$DomainController,
@@ -17,24 +17,43 @@ function Get-GPOConsistencyBetweenDirectoryAndSysvol {
         $domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().Name
     }
 
-    [System.Collections.Generic.List[PSCustomObject]]$gpoResults = @()
+    [System.Collections.Generic.List[PSCustomObject]]$gpoConsistencyArray = @()
 
     foreach ($DC in $DomainController) {
-        [array]$GPOs = @(Get-GPO -All -Server $DC)
-
+        try {   
+            [array]$GPOs = @(Get-GPO -All -Server $DC -ErrorAction Stop)
+        }
+        catch {
+            $object = [PSCustomObject][ordered]@{
+                FromDC = $DC
+                Name   = $_.Exception.Message
+                Status = '-'
+                GUID   = '-'
+            }
+    
+            $gpoConsistencyArray.Add($object)
+            continue
+        }
         try {
             $SYSVOL = Get-ChildItem -Path "\\$DC\SYSVOL\$Domain\Policies" -ErrorAction Stop | Where-Object { $_.PsIsContainer -and $_.FullName -notmatch 'PolicyDefinitions' }
         }
         catch {
-            Write-Warning "Error connecting to 'SYSVOL' folder on ($DC). Fix this problem and retry"
-            break
+            $object = [PSCustomObject][ordered]@{
+                FromDC = $DC
+                Name   = $_.Exception.Message
+                Status = '-'
+                GUID   = '-'
+            }
+        
+            $gpoConsistencyArray.Add($object)
+            continue
         }
 
         if (Test-Path -Path "\\$DC\SYSVOL\$Domain\Policies\PolicyDefinitions" -ErrorAction SilentlyContinue) {
-            Write-Host -ForegroundColor green 'PolicyDefinitions is present!'
+            Write-Host -ForegroundColor green "$DC - PolicyDefinitions is present!"
         }
         else {
-            Write-Warning 'PolicyDefinitions folder is not present, considere to create one to keep consistency in ADM/ADMX.'
+            Write-Warning "$DC - PolicyDefinitions folder is not present, considere to create one to keep consistency in ADM/ADMX."
         }    
  
         [array]$SYSVOLIds = ((($SYSVOL).Name) -replace '{', '') -replace '}', ''
@@ -58,22 +77,15 @@ function Get-GPOConsistencyBetweenDirectoryAndSysvol {
                     $Name = 'N/A'
                 }
 
-                $object = New-Object -TypeName PSObject -Property @{
+                $object = [PSCustomObject][ordered]@{
                     FromDC = $DC
                     Name   = $Name
                     Status = $Status
                     GUID   = $ID
                 }
 
-                $gpoResults.Add($object)
+                $gpoConsistencyArray.Add($object)
             }
-
-            Write-Host -ForegroundColor green "Below information taken from ($DC) :
-    SysVol: $CountSysvol
-    GPO   : $CountGPOs
-
-It seems you have no inconsistency issues between SYSVOL and GPOs on your DC $DC." 
-
         }
 
         elseif ($CountSysvol -gt $CountGPOs) {
@@ -92,24 +104,15 @@ It seems you have no inconsistency issues between SYSVOL and GPOs on your DC $DC
                     $Name = 'N/A'
                 }
     
-                $object = New-Object -TypeName PSObject -Property @{
+                $object = [PSCustomObject][ordered]@{
                     FromDC = $DC
                     Name   = $Name
                     Status = $Status
                     GUID   = $ID
                 }
             
-                $gpoResults.Add($object)
+                $gpoConsistencyArray.Add($object)
             }
-
-            $countPhantoms = ($gpoResults | Where-Object { $_.Status -eq 'Phantom' -or $_.Status -eq 'Missing' }).count
-            Write-Warning "Below information taken from $DC :
-
-    SysVol: $CountSysvol
-    GPO   : $CountGPOs
-
-You probably need to carefuly remove items marked with 'Phantom' from you SYSVOL. There are about $CountPhantoms phantom folders in SYSVOL directory of $DC"
-
         }
 
         elseif ($CountSysvol -lt $CountGPOs) {
@@ -125,27 +128,17 @@ You probably need to carefuly remove items marked with 'Phantom' from you SYSVOL
                     $Status = 'Folder Missing'
                 }
 
-                $object = New-Object -TypeName PSObject -Property @{
+                $object = [PSCustomObject][ordered]@{
                     FromDC = $DC
                     Name   = $Name
                     Status = $Status
                     GUID   = $ID
                 }
 
-                $gpoResults.Add($object)
+                $gpoConsistencyArray.Add($object)
             }
         }
-
-        Write-Warning  "Below information taken from $DC :
-
-    SysVol: $CountSysvol
-    GPO   : $CountGPOs
-
-It seems there are problems with some GPOs. There is a high chance that you have GPOs where there is no associated folder for them in SYSVOL directory of $DC.
-
-Below policies are missing from SYSVOL of PDC Emulator. Possible solutions are restoring this GPOS from backup and import it on PDC or, change the PDC to the DC who has these missing GPOs in their SYSVOL:
-    "
     }
 
-    $gpoResults
+    $gpoConsistencyArray
 }
