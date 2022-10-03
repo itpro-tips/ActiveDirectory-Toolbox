bd909e48d794ab608979b8c4fdf0e13ba56e0762
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2022-10-03 09:26:47 +0200

    Create Get-ComputerPublicIPAddress

---
+++ System/Get-ComputerPublicIPAddress.ps1
@@ -0,0 +1,49 @@
+function Get-ComputerPublicIPAddress {
+    <#
+    .SYNOPSIS
+    Get the public IP address of a computer
+    .PARAMETER ComputerName
+    Remote computer (use WSMan)
+
+    .EXAMPLE
+    Get-ComputerPublicIPAddress
+    
+    .EXAMPLE
+    Get-ComputerPublicIPAddress -ComputerName DC01
+
+    #>
+    
+    Param (
+        [String]$ComputerName
+    )
+
+    # use mullvad because no API key needed and return : ip, country, city, longitude, latitude, blacklisted, organization (ISP)
+    $getIP = 'https://am.i.mullvad.net/json'
+
+    if ($ComputerName) {
+        $ipAddress = Invoke-Command -ComputerName $ComputerName { Invoke-RestMethod -Method Get -Uri $args[0] } -ArgumentList $getIP
+    }
+    else {
+        $ipAddress = Invoke-RestMethod -Method Get -Uri $getIP
+    }
+   
+    [System.Collections.Generic.List[String]]$blackListedArray = @()
+
+    foreach ($blacklist in $ipAddress.blacklisted.results) {
+        $blackListedArray.Add("$($blacklist.name)=$($blacklist.blackListed)")
+    }
+
+    $object = [PSCustomObject][ordered] @{
+        IP               = $ipAddress.ip
+        Country          = $ipAddress.country
+        City             = $ipAddress.city
+        Longitude        = $ipAddress.longitude
+        Latitude         = $ipAddress.latitude
+        Blacklisted      = if (($ipAddress.blacklisted.results | Where-Object { $_.blacklisted}).count -gt 0) { $true } else { $false }
+        BlacklistDetails = $blackListedArray -join '|'
+        Organization     = $ipAddress.organization
+        GMaps            = "https://maps.google.com/?q=$($ipAddress.latitude),$($ipAddress.longitude)"
+    }
+
+    return $object
+}
