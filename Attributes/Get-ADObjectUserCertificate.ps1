8f2fc9f9edddd1bb4e141550b7f821709fffb5c3
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2022-12-20 15:18:07 +0100

    Update Get-ADObjectUsercertificate

--- Attributes/Get-ADObjectUserCertificate.ps1
+++ Attributes/Get-ADObjectUserCertificate.ps1
@@ -1,55 +1,66 @@
 function Get-ADObjectUserCertificate {
-    # Parameter help description
-    [String]$Domain
-
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [String]$DistinguishedName,
+        [Parameter(Mandatory = $false)]
+        [String]$DomainController
+    )    
+    
     [System.Collections.Generic.List[PSObject]]$certificatesArray = @()
 
-    if ($domain) {
-        $adObjectsWithCertificate = Get-ADObject -Filter { UserCertificate -like '*' -or userCert -like '*' -or UserSMIMECertificate -like '*' } -Properties * -Server $Domain    
+    if (-not ($DomainController)) {
+        $DomainController = $env:USERDNSDOMAIN
+    }    
+
+    if ($DistinguishedName) {
+        $adObjectsWithCertificate = Get-ADObject -Identity $DistinguishedName -Properties * -Server $DomainController
     }
     else {
-        $adObjectsWithCertificate = Get-ADObject -Filter { UserCertificate -like '*' -or userCert -like '*' -or UserSMIMECertificate -like '*' } -Properties *
+        $adObjectsWithCertificate = Get-ADObject -Filter { UserCertificate -like '*' -or userCert -like '*' -or UserSMIMECertificate -like '*' } -Properties *  -Server $DomainController
     }
 
     foreach ($adObject in $ADObjectsWithCertificate) {
-        $certs = @()
+        [System.Collections.Generic.List[PSObject]]$certificates = @()
 
         if ($adObject.UserCertificate) {
             $adObject | Select-Object -ExpandProperty usercertificate | ForEach-Object {
-                $certs = + [System.Security.Cryptography.X509Certificates.X509Certificate2]$_
+                $certificates.Add([System.Security.Cryptography.X509Certificates.X509Certificate2]$_)
             }
         }
         if ($adObject.UserSMIMECertificate) {
             $adObject | Select-Object -ExpandProperty UserSMIMECertificate | ForEach-Object {
-                $certs = + [System.Security.Cryptography.X509Certificates.X509Certificate2]$_
+                $certificates.Add([System.Security.Cryptography.X509Certificates.X509Certificate2]$_)
             }
         }
         if ($adObject.userCert) {
             $adObject | Select-Object -ExpandProperty UserSMIMECertificate | ForEach-Object {
-                $certs = + [System.Security.Cryptography.X509Certificates.X509Certificate2]$_
+                $certificates.Add([System.Security.Cryptography.X509Certificates.X509Certificate2]$_)
             }
         }
 
-        foreach ($cert in $certs) {
+        foreach ($certificate in $certificates) {
             $object = [PSCustomObject][ordered]@{
                 Name               = $adObject.Name
                 DisplayName        = $adObject.displayname
                 DN                 = $adObject.DistinguishedName
-                IssuedTo           = $cert.Subject
-                IssuedBy           = $cert.Issuer
-                IntendedPurpose    = $cert.EnhancedKeyUsageList
-                NotBefore          = $cert.NotBefore
-                NotAfter           = $cert.NotAfter
-                SerialNumber       = $cert.SerialNumber
-                Thumbprint         = $cert.Thumbprint
+                IssuedTo           = $certificate.Subject
+                IssuedBy           = $certificate.Issuer
+                IntendedPurpose    = $certificate.EnhancedKeyUsageList
+                NotBefore          = $certificate.NotBefore
+                NotAfter           = $certificate.NotAfter
+                SerialNumber       = $certificate.SerialNumber
+                Thumbprint         = $certificate.Thumbprint
                 ObjectClass        = $adObject.ObjectClass
-                CertDnsNameList    = $cert.DnsNameList
-                IssuerName         = $cert.IssuerName.Name
-                SubjectName        = $cert.SubjectName.Name
-                SignatureAlgorithm = $cert.SignatureAlgorithm.FriendlyName
+                CertDnsNameList    = $certificate.DnsNameList
+                IssuerName         = $certificate.IssuerName.Name
+                SubjectName        = $certificate.SubjectName.Name
+                SignatureAlgorithm = $certificate.SignatureAlgorithm.FriendlyName
             }
 
             $certificatesArray.Add($object)
         }
     }
+
+    return $certificatesArray
 }
