3413e45af84a1cfa7ec2e748f4be3d6fea97d2f4
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2023-02-08 10:14:22 +0100

    Update Get-HighPrivilegedGroupsMembers.ps1

--- PrivilegedAccounts/Get-HighPrivilegedGroupsMembers.ps1
+++ PrivilegedAccounts/Get-HighPrivilegedGroupsMembers.ps1
@@ -1,18 +1,31 @@
 [CmdletBinding()]
 param (
-    [Parameter()]
-    [String]$DomainController
+    [Parameter(Mandatory = $false)]
+    [String]$DomainController,
+    [Parameter(Mandatory = $false)]
+    [Switch]$ExportCSV
 )
 
-[System.Collections.Generic.List[PSObject]] $high = @()
+# exit if Active Directory powershell module does not installed
+try {
+    Import-Module ActiveDirectory -ErrorAction Stop
+}
+catch {
+    Write-Warning "ActiveDirectory module not found. Please install it and try again."
+    return
+}
+
+[System.Collections.Generic.List[PSObject]] $highPrivilegedArray = @()
+$exportFolder = 'c:\temp'
 
 if (-not $DomainController) {
-    $DomainController = (Get-ADDomainController -Discover).HostName
+    # cast an array in case only one DC is found
+    $DomainController = @(Get-ADDomainController -Discover).HostName
 }
     
 $currentDomainSID = (Get-ADDomain -Server $DomainController).DomainSID
 $highPrivilegedGroups = Get-ADGroup -Filter { adminCount -eq '1' } -Server $DomainController
-$protectedusers = Get-ADGroupMember "$($currentDomainSID.Value)-525" -Server $DomainController
+$protectedUsersGroup = Get-ADGroupMember "$($currentDomainSID.Value)-525" -Server $DomainController -Recursive
 
 foreach ($group in $highPrivilegedGroups) {
     $members = Get-ADGroupMember -Identity $group.DistinguishedName -Recursive -Server $DomainController
@@ -34,27 +47,47 @@ foreach ($group in $highPrivilegedGroups) {
         }
     
         $object = [PSCustomObject][ordered]@{
-            GroupName                                                                 = $group.Name
-            SamAccountName                                                            = $member.SamAccountName
-            Enabled                                                                   = if ($member.Enabled) { $true } else { $false }
-            Active                                                                    = if ($(Get-Date).AddDays(-90) -lt $member.lastLogonDate ) { $true } else { $false }
-            'Pwd never Expired'                                                       = if ($member.PasswordNeverExpires) { $true } else { $false }
-            'Locked'                                                                  = if ($member.LockedOut) { $true } else { $false }
-            'Smart Card required'                                                     = if ($member.SmartcardLogonRequired) { $true } else { $false }
-            'Service account (has SPN attribute used now or in the past for service)' = if ($member.ServicePrincipalName -like '*') { $true } else { $false }
-            'Flag Cannot be delegated present'                                        = if ($member.AccountNotDelegated) { $true } else { $false }
-            'Creation date'                                                           = $member.whenCreated
-            'Last login'                                                              = if ($member.lastLogonDate) { $member.lastLogonDate } else { 'Never' }
-            'Password last set'                                                       = $member.PasswordLastSet
-            'In Protected Users'                                                      = if ($protectedusers.DistinguishedName -contains $member.DistinguishedName) { $true } else { $false }
-            'Distinguished name'                                                      = $member.DistinguishedName
-            DirectMember                                                              = $directMember
-            ObjectClass                                                               = $member.ObjectClass
-            OperatingSystem                                                           = if ($member.OperatingSystem) { $member.OperatingSystem } else { '-' }
+            GroupName                            = $group.Name
+            SamAccountName                       = $member.SamAccountName
+            Enabled                              = if ($member.Enabled) { $true } else { $false }
+            'Active[Last90Days]'                 = if ($(Get-Date).AddDays(-90) -lt $member.lastLogonDate ) { $true } else { $false }
+            'Pwd never Expired'                  = if ($member.PasswordNeverExpires) { $true } else { $false }
+            'Locked'                             = if ($member.LockedOut) { $true } else { $false }
+            'Smart Card required'                = if ($member.SmartcardLogonRequired) { $true } else { $false }
+            'Service account[has SPN attribute]' = if ($member.ServicePrincipalName -like '*') { $true } else { $false }
+            'Flag Cannot be delegated present'   = if ($member.AccountNotDelegated) { $true } else { $false }
+            'Creation date'                      = $member.whenCreated
+            'Last login'                         = if ($member.lastLogonDate) { $member.lastLogonDate } else { 'Never' }
+            'Password last set'                  = $member.PasswordLastSet
+            'In Protected Users'                 = if ($protectedUsersGroup.DistinguishedName -contains $member.DistinguishedName) { $true } else { $false }
+            'Distinguished name'                 = $member.DistinguishedName
+            DirectMember                         = $directMember
+            ObjectClass                          = $member.ObjectClass
+            OperatingSystem                      = if ($member.OperatingSystem) { $member.OperatingSystem } else { '-' }
         }
 
-        $high.Add($object)
+        $highPrivilegedArray.Add($object)
     }
 }
 
-return $high
+if ($ExportCSV) {
+    $now = (Get-Date).ToString('yyyyMMdd')
+    
+    if (-not(Test-Path $exportFolder)) {
+        try {
+            $null = New-Item -Path $exportFolder -ItemType Directory
+        }
+        catch {
+            Write-Warning "Could not create exportFolder $exportFolder. $($_.Exception.Message)"
+            return
+        }
+    }
+
+    $csvFilePath = "$exportFolder\$now-HighPrivilegedGroupsMembers.csv"
+    $highPrivilegedArray | Export-Csv -Path $csvFilePath -NoTypeInformation -Encoding UTF8
+
+    Write-Host "Exported to $csvFilePath"
+}
+else {
+    return $highPrivilegedArray
+}
