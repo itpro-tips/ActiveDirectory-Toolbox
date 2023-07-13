3207c3b831b2e86e203996760ea29e36c12349c7
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2023-07-13 08:55:55 +0200

    modify schema

--- Schema/Get-ADAttributesFromClassName.ps1
+++ Schema/Get-ADAttributesFromClassName.ps1
@@ -1,38 +1,45 @@
-# Source https://www.easy365manager.com/how-to-get-all-active-directory-user-object-attributes/
+# modified version of https://www.easy365manager.com/how-to-get-all-active-directory-user-object-attributes/
 Function Get-ADAttributesFromClassName {
     Param(
         [CmdletBinding()]
-    
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory = $true)]
         [String]$ClassName
     )
-    $Loop = $True
-    $classArray = New-Object -TypeName 'System.Collections.ArrayList'
-    $attributes = New-Object -TypeName 'System.Collections.ArrayList'
+
+    $loop = $True
+    [System.Collections.Generic.List[PSObject]]$classArray = @()
+    [System.Collections.Generic.List[PSObject]]$attributesArray = @()
     
     # Retrieve the object class and any parent classes
-    While ($Loop) {
+    
+    while ($loop) {
         $class = Get-ADObject -SearchBase (Get-ADRootDSE).SchemaNamingContext -Filter { ldapDisplayName -Like $ClassName } -Properties AuxiliaryClass, SystemAuxiliaryClass, mayContain, mustContain, systemMayContain, systemMustContain, subClassOf, ldapDisplayName
-        If ($class.ldapDisplayName -eq $class.subClassOf) {
-            $Loop = $False
+        
+        if ($class.ldapDisplayName -eq $class.subClassOf) {
+            $loop = $False
         }
         
         $null = $ClassArray.Add($class)
+
         $ClassName = $class.subClassOf
     }
+    
     # Loop through all the classes and get all auxiliary class attributes and direct attributes
     $ClassArray | ForEach-Object {
+
         # Get Auxiliary class attributes
-        $Aux = $_.AuxiliaryClass | ForEach-Object { 
-            Get-ADObject -SearchBase (Get-ADRootDSE).SchemaNamingContext -Filter { ldapDisplayName -like $_ } -Properties mayContain, mustContain, systemMayContain, systemMustContain } | Select-Object @{n = "Attributes"; e = { $_.mayContain + $_.mustContain + $_.systemMaycontain + $_.systemMustContain } } | Select-Object -ExpandProperty Attributes
+        $auxiliaryClass = $_.AuxiliaryClass | ForEach-Object { 
+            Get-ADObject -SearchBase (Get-ADRootDSE).SchemaNamingContext -Filter { ldapDisplayName -like $_ } -Properties mayContain, mustContain, systemMayContain, systemMustContain } | Select-Object @{Name = 'Attributes'; Expression = { $_.mayContain + $_.mustContain + $_.systemMaycontain + $_.systemMustContain } } | Select-Object -ExpandProperty Attributes
+        
         # Get SystemAuxiliary class attributes
         if ($UserClass.SystemAuxiliaryClass.count -ge 1) {
-            $SysAux = $UserClass.SystemAuxiliaryClass | ForEach-Object {
-                Get-ADObject -ErrorAction SilentlyContinue -SearchBase (Get-ADRootDSE).SchemaNamingContext -Filter { ldapDisplayName -like $_ } -Properties MayContain, SystemMayContain, systemMustContain } | Select-Object @{n = "Attributes"; e = { $_.maycontain + $_.systemmaycontain + $_.systemMustContain } } | Select-Object -ExpandProperty Attributes
+            $SystemAuxiliaryClass = $UserClass.SystemAuxiliaryClass | ForEach-Object {
+                Get-ADObject -ErrorAction SilentlyContinue -SearchBase (Get-ADRootDSE).SchemaNamingContext -Filter { ldapDisplayName -like $_ } -Properties MayContain, SystemMayContain, systemMustContain } | Select-Object @{Name = 'Attributes'; Expression = { $_.maycontain + $_.systemmaycontain + $_.systemMustContain } } | Select-Object -ExpandProperty Attributes
         }
+
         # Get direct attributes
-        $attributes += $Aux + $SysAux + $_.mayContain + $_.mustContain + $_.systemMayContain + $_.systemMustContain
+        $attributesArray.Add($auxiliaryClass + $SystemAuxiliaryClass + $_.mayContain + $_.mustContain + $_.systemMayContain + $_.systemMustContain)
     }
     
-    return $attributes | Sort-Object | Get-Unique
+    return $attributesArray | Sort-Object | Get-Unique
 }
--- Schema/Get-RelatedClass.ps1
+++ Schema/Get-RelatedClass.ps1
@@ -1,19 +1,26 @@
-# https://www.neroblanco.co.uk/2017/09/get-possible-ad-attributes-user-group/
+# modified version of https://www.neroblanco.co.uk/2017/09/get-possible-ad-attributes-user-group/
 
 Function Get-RelatedClass {
-  param( [string]$ClassName )
-  
+  Param(
+    [CmdletBinding()]
+    [Parameter(Mandatory = $true)]
+    [String]$ClassName
+)
+
   $Classes = @($ClassName)
   
-  $SubClass = Get-ADObject -SearchBase "$((Get-ADRootDSE).SchemaNamingContext)" -Filter {lDAPDisplayName -eq $ClassName} -properties subClassOf |Select-Object -ExpandProperty subClassOf
+  $SubClass = Get-ADObject -SearchBase "$((Get-ADRootDSE).SchemaNamingContext)" -Filter {lDAPDisplayName -eq $ClassName} -properties subClassOf | Select-Object -ExpandProperty subClassOf
+  
   if( $Subclass -and $SubClass -ne $ClassName ) {
     $Classes += Get-RelatedClass $SubClass
   }
   
   $auxiliaryClasses = Get-ADObject -SearchBase "$((Get-ADRootDSE).SchemaNamingContext)" -Filter {lDAPDisplayName -eq $ClassName} -properties auxiliaryClass | Select-Object -ExpandProperty auxiliaryClass
+  
   foreach( $auxiliaryClass in $auxiliaryClasses ) {
     $Classes += Get-RelatedClass $auxiliaryClass
   }
 
-  Return $Classes  
+  return $Classes
+   
 }
