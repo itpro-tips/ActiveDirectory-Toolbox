d157f84555233bf4a9505425ad4b26a4355b8827
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2023-09-04 18:37:52 +0200

    Update Invoke-ADSDPropagation.ps1

--- AdminCount/Invoke-ADSDPropagation.ps1
+++ AdminCount/Invoke-ADSDPropagation.ps1
@@ -1,4 +1,4 @@
-Function Invoke-ADSDPropagation{
+Function Invoke-ADSDPropagation {
     <#
     .SYNOPSIS
         Invoke a SDProp task on the PDCe.
@@ -14,37 +14,32 @@ Function Invoke-ADSDPropagation{
         Name of the task to use.
             - FixUpInheritance for legacy OS (2008 or earlier)
             - RunProtectAdminGroupsTask for recent OS (from 2008 R2)
-    .INPUTS
-    .OUTPUTS
     .NOTES
         You can track progress with:
         Get-Counter -Counter '\directoryservices(ntds)\ds security descriptor propagator runtime queue' | Select-Object -ExpandProperty CounterSamples | Select-Object -ExpandProperty CookedValue
     .LINK
-        http://ItForDummies.net
     #>
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory=$false,
-            HelpMessage='Name of the domain where to force SDProp to run',
-            Position=0)]
-        [ValidateScript({Test-Connection -ComputerName $_ -Count 2 -Quiet})]
-        [String]$DomainName = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().Name,
+        [Parameter(Mandatory = $false,
+            HelpMessage = 'Name of the domain where to force SDProp to run')]
+        [String]$domainName = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().Name,
 
-        [ValidateSet('RunProtectAdminGroupsTask','FixUpInheritance')]
+        [ValidateSet('RunProtectAdminGroupsTask', 'FixUpInheritance')]
         [String]$TaskName = 'RunProtectAdminGroupsTask'
     )
 
-    try{
-	$DomainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('domain',$DomainName)
-        $DomainObject = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($DomainContext)
+    try {
+        $domainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('domain', $domainName)
+        $domainObject = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($domainContext)
         
-        Write-Verbose -Message "Detected PDCe is $($DomainObject.PdcRoleOwner.Name)."
-        $RootDSE = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$($DomainObject.PdcRoleOwner.Name)/RootDSE") 
-        $RootDSE.UsePropertyCache = $false 
-        $RootDSE.Put($TaskName, "1") # RunProtectAdminGroupsTask & fixupinheritance
-        $RootDSE.SetInfo()
+        Write-Verbose -Message "Detected PDCe is $($domainObject.PdcRoleOwner.Name)."
+        $rootDSE = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$($domainObject.PdcRoleOwner.Name)/RootDSE") 
+        $rootDSE.UsePropertyCache = $false 
+        $rootDSE.Put($TaskName, "1") # RunProtectAdminGroupsTask & fixupinheritance
+        $rootDSE.SetInfo()
     }
-    catch{
-        throw "Can't invoke SDProp on $($DomainObject.PdcRoleOwner.Name) !"
+    catch {
+        throw "Can't invoke SDProp on $($domainObject.PdcRoleOwner.Name) !"
     }
 }
