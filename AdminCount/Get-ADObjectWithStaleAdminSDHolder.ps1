88bf2ff207612ae4f80adcaef59e0182358e8078
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2023-10-18 18:04:25 +0200

    Update Get-ADObjectWithStaleAdminSDHolder.ps1

--- AdminCount/Get-ADObjectWithStaleAdminSDHolder.ps1
+++ AdminCount/Get-ADObjectWithStaleAdminSDHolder.ps1
@@ -1,55 +1,58 @@
-[System.Collections.Generic.List[PSObject]]$OrphanResults = @()
-[System.Collections.Generic.List[PSObject]]$NonOrphanResults = @()
-[System.Collections.Generic.List[PSObject]]$results = @()
-[System.Collections.Generic.List[PSObject]]$objectsProtectedByAdminSDHolder = @()
+function Get-ADObjectWithStaleAdminSDHolder {
 
-Get-ADObject -Filter 'admincount -eq 1 -and (iscriticalsystemobject -ne $TRUE -or iscriticalsystemobject -notlike "*")' `
-    -Properties whenchanged, whencreated, admincount, isCriticalSystemObject, "msDS-ReplAttributeMetaData", samaccountname | ForEach-Object {
-    $object = [PSCustomObject]@{
-        distinguishedname      = $_.distinguishedname
-        whenchanged            = $_.whenchanged
-        whencreated            = $_.whencreated
-        admincount             = $_.admincount
-        SamAccountName         = $_.SamAccountName
-        objectclass            = $_.objectclass
-        isCriticalSystemObject = $_.isCriticalSystemObject
-        adminCountDate         = ($_.msDSReplAttributeMetaData | Where-Object { $_.pszAttributeName -eq 'admincount' }).ftimeLastOriginatingChange | Get-Date -Format MM/dd/yyyy
-    }
+    [System.Collections.Generic.List[PSObject]]$OrphanResults = @()
+    [System.Collections.Generic.List[PSObject]]$NonOrphanResults = @()
+    [System.Collections.Generic.List[PSObject]]$results = @()
+    [System.Collections.Generic.List[PSObject]]$objectsProtectedByAdminSDHolder = @()
 
-    $objectsProtectedByAdminSDHolder.Add($object)
-}
-
-$defaultAdminGroups = Get-ADGroup -Filter 'admincount -eq 1 -and iscriticalsystemobject -like "*"' | Select-Object distinguishedname
-
-foreach ($objectProtected in $objectProtectedByAdminSDHolder) {
-    $objectDN = ($objectProtected).Distinguishedname
-    foreach ($Group in $DefaultAdminGroups) {
+    Get-ADObject -Filter 'admincount -eq 1 -and (iscriticalsystemobject -ne $TRUE -or iscriticalsystemobject -notlike "*")' `
+        -Properties whenchanged, whencreated, admincount, isCriticalSystemObject, "msDS-ReplAttributeMetaData", samaccountname | ForEach-Object {
         $object = [PSCustomObject]@{
-            GroupDistinguishedname = $Group.distinguishedname
-            Member                 = if (Get-ADgroup -Filter { member -RecursiveMatch $objectDN } -searchbase $Group.distinguishedname) { $True } else { $False }
-            distinguishedname      = $Object.distinguishedname
-            admincount             = $Object.admincount
-            adminCountDate         = $Object.adminCountDate
-            whencreated            = $Object.whencreated
-            objectclass            = $Object.objectclass
+            distinguishedname      = $_.distinguishedname
+            whenchanged            = $_.whenchanged
+            whencreated            = $_.whencreated
+            admincount             = $_.admincount
+            SamAccountName         = $_.SamAccountName
+            objectclass            = $_.objectclass
+            isCriticalSystemObject = $_.isCriticalSystemObject
+            adminCountDate         = ($_.msDSReplAttributeMetaData | Where-Object { $_.pszAttributeName -eq 'admincount' }).ftimeLastOriginatingChange | Get-Date -Format MM/dd/yyyy
         }
 
-        $results.Add($object)
+        $objectsProtectedByAdminSDHolder.Add($object)
     }
 
-    if ($Results | Where-Object { $_.member }) {
-        $NonOrphanResults.Add($($Results | Where-Object { $_.member }))
+    $defaultAdminGroups = Get-ADGroup -Filter 'admincount -eq 1 -and iscriticalsystemobject -like "*"' | Select-Object distinguishedname
+
+    foreach ($objectProtected in $objectProtectedByAdminSDHolder) {
+        $objectDN = ($objectProtected).Distinguishedname
+        foreach ($Group in $DefaultAdminGroups) {
+            $object = [PSCustomObject]@{
+                GroupDistinguishedname = $Group.distinguishedname
+                Member                 = if (Get-ADgroup -Filter { member -RecursiveMatch $objectDN } -searchbase $Group.distinguishedname) { $True } else { $False }
+                distinguishedname      = $Object.distinguishedname
+                admincount             = $Object.admincount
+                adminCountDate         = $Object.adminCountDate
+                whencreated            = $Object.whencreated
+                objectclass            = $Object.objectclass
+            }
+
+            $results.Add($object)
+        }
+
+        if ($Results | Where-Object { $_.member }) {
+            $NonOrphanResults.Add($($Results | Where-Object { $_.member }))
+        }
+        else {
+            $OrphanResults.Add($($Results  | Select-Object objectclass, admincount, adminCountDate, distinguishedname | Get-Unique))
+        }
+    }
+
+    if ($OrphanResults) {
+        "Found $(($OrphanResults | Measure-Object).count) user object that are no longer a member of a priviledged group but still has admincount attribute set to 1 and inheritance disabled"
     }
     else {
-        $OrphanResults.Add($($Results  | Select-Object objectclass, admincount, adminCountDate, distinguishedname | Get-Unique))
+        'Found 0 Objects with Stale Admin Count'
     }
-}
 
-if ($OrphanResults) {
-    "Found $(($OrphanResults | Measure-Object).count) user object that are no longer a member of a priviledged group but still has admincount attribute set to 1 and inheritance disabled"
+    return $OrphanResults
 }
-else {
-    'Found 0 Objects with Stale Admin Count'
-}
-
-return $OrphanResults
