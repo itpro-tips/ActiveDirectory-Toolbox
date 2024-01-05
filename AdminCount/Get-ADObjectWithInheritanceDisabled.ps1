d8ad0b0946ea6b6e5043d1f788e6489a1d33c637
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2024-01-05 15:31:37 +0100

    Update Get-ADObjectWithInheritanceDisabled.ps1

--- AdminCount/Get-ADObjectWithInheritanceDisabled.ps1
+++ AdminCount/Get-ADObjectWithInheritanceDisabled.ps1
@@ -1,5 +1,18 @@
-$adObjects = Get-ADObject -LDAPFilter '(&(!(adminCount=1))(|(ObjectClass=user)(ObjectClass=Computer)))' -SearchBase (Get-ADRootDSE).defaultNamingContext -Properties *| where {$_.nTSecurityDescriptor.AreAccessRulesProtected}
+$adObjects = Get-ADObject -LDAPFilter '(&(!(adminCount=1))(|(ObjectClass=user)(ObjectClass=Computer)))' -SearchBase (Get-ADRootDSE).defaultNamingContext -Properties DistinguishedName, nTSecurityDescriptor | Where-Object { $_.nTSecurityDescriptor.AreAccessRulesProtected }
 
-foreach($adObject in $adObjects){
-    Write-Warning "Inheritance disabled $($adObject.DistinguishedName)"
+[System.Collections.Generic.List[PSObject]]$adobjectWithInheritanceDisabled = @()
+
+foreach ($adObject in $adObjects) {
+    $adobjectWithInheritanceDisabled.Add($adObject)
+}
+
+if ($adobjectWithInheritanceDisabled) {
+    Write-Host -ForegroundColor Yellow "Found $($adobjectWithInheritanceDisabled.count) object(s) that are no longer a member of a priviledged group but still has admincount attribute set to 1 and inheritance disabled."
+    Write-Host -ForegroundColor Yellow "To re-enable inheritance and remove AdminAccount you can for example use https://github.com/itpro-tips/ActiveDirectory-Toolbox/blob/master/AdminCount/Enable-ACLInheritance.ps1):"
+    Write-Host -ForegroundColor Yellow "Please review the following objects carefully to identify if you need to re-enable inheritance and remove AdminAccount:"
+
+    return $adobjectWithInheritanceDisabled.DistinguishedName
+}
+else {
+    Write-Host -ForegroundColor Green 'Found 0 Objects with Stale Admin Count'
 }
