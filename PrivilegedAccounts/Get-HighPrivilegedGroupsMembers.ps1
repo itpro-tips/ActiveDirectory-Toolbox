f9885ca9a254ed7105869ae0a5844d5a18ce70f5
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2024-01-10 17:05:30 +0100

    Update Get-HighPrivilegedGroupsMembers.ps1

--- PrivilegedAccounts/Get-HighPrivilegedGroupsMembers.ps1
+++ PrivilegedAccounts/Get-HighPrivilegedGroupsMembers.ps1
@@ -46,25 +46,26 @@ function Get-HighPrivilegedGroupsMembers {
             else {
                 Write-Warning "$($member.Objectclass) not known by this script"
             }
-    
+
             $object = [PSCustomObject][ordered]@{
-                GroupName                            = $group.Name
-                SamAccountName                       = $member.SamAccountName
-                Enabled                              = if ($member.Enabled) { $true } else { $false }
-                'Active[Last90Days]'                 = if ($(Get-Date).AddDays(-90) -lt $member.lastLogonDate ) { $true } else { $false }
-                'Pwd never Expired'                  = if ($member.PasswordNeverExpires) { $true } else { $false }
-                'Locked'                             = if ($member.LockedOut) { $true } else { $false }
-                'Smart Card required'                = if ($member.SmartcardLogonRequired) { $true } else { $false }
-                'Service account[has SPN attribute]' = if ($member.ServicePrincipalName -like '*') { $true } else { $false }
-                'Flag Cannot be delegated present'   = if ($member.AccountNotDelegated) { $true } else { $false }
-                'Creation date'                      = $member.whenCreated
-                'Last login'                         = if ($member.lastLogonDate) { $member.lastLogonDate } else { 'Never' }
-                'Password last set'                  = $member.PasswordLastSet
-                'In Protected Users'                 = if ($protectedUsersGroup.DistinguishedName -contains $member.DistinguishedName) { $true } else { $false }
-                'Distinguished name'                 = $member.DistinguishedName
-                DirectMember                         = $directMember
-                ObjectClass                          = $member.ObjectClass
-                OperatingSystem                      = if ($member.OperatingSystem) { $member.OperatingSystem } else { '-' }
+                GroupName                          = $group.Name
+                SamAccountName                     = $member.SamAccountName
+                Enabled                            = if ($member.Enabled) { $true } else { $false }
+                'Active[Last90Days]'               = if ($(Get-Date).AddDays(-90) -lt $member.lastLogonDate ) { $true } else { $false }
+                'Pwd never Expired'                = if ($member.PasswordNeverExpires) { $true } else { $false }
+                'Locked'                           = if ($member.LockedOut) { $true } else { $false }
+                'Smart Card required'              = if ($member.SmartcardLogonRequired) { $true } else { $false }
+                'User kerboastable'                = if ($member.ServicePrincipalName -and $member.ObjectClass -eq 'User') { $true } else { $false }
+                'ServicePrincipalNames'            = $member.ServicePrincipalName -join '|'
+                'Flag Cannot be delegated present' = if ($member.AccountNotDelegated) { $true } else { $false }
+                'Creation date'                    = $member.whenCreated
+                'Last login'                       = if ($member.lastLogonDate) { $member.lastLogonDate } else { 'Never' }
+                'Password last set'                = $member.PasswordLastSet
+                'In Protected Users'               = if ($protectedUsersGroup.DistinguishedName -contains $member.DistinguishedName) { $true } else { $false }
+                'Distinguished name'               = $member.DistinguishedName
+                DirectMember                       = $directMember
+                ObjectClass                        = $member.ObjectClass
+                OperatingSystem                    = if ($member.OperatingSystem) { $member.OperatingSystem } else { '-' }
             }
 
             $highPrivilegedArray.Add($object)
