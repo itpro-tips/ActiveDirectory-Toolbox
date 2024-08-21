06f6978773afc638865a10c8e0e6ada12af210f1
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2024-08-21 15:40:21 +0200

    Refactor Get-MasterOperationRoles.ps1 to improve application partition handling

--- Get-MasterOperationRoles.ps1
+++ Get-MasterOperationRoles.ps1
@@ -6,23 +6,26 @@ function Get-MasterOperationRoles {
     $adForest = Get-ADForest
     $adDomain = Get-ADDomain
 
-    $domainDnsZonesOwner = (Get-ADObject "CN=Infrastructure,DC=DomainDnsZones,$($adDomain.DistinguishedName)" -Properties fsmoRoleOwner).fsmoRoleOwner
-    $forestDnsZonesOwner = (Get-ADObject "CN=Infrastructure,DC=ForestDnsZones,$($adDomain.DistinguishedName)" -Properties fsmoRoleOwner).fsmoRoleOwner
-
-    if ($ConvertNTDSToHostName.IsPresent) {
-        $domainDnsZonesOwner = (Get-ADDomainController -Filter { NTDSSettingsObjectDN -eq $domainDnsZonesOwner }).HostName
-        $forestDnsZonesOwner = (Get-ADDomainController -Filter { NTDSSettingsObjectDN -eq $forestDnsZonesOwner }).HostName
-    }
-    
     $object = [PSCustomObject][ordered] @{
         DomainNamingMaster   = $adForest.DomainNamingMaster
         SchemaMaster         = $adForest.SchemaMaster
         InfrastructureMaster = $adDomain.InfrastructureMaster
         RIDMaster            = $adDomain.RIDMaster
         PDCEmulator          = $adDomain.PDCEmulator
-        DomainDnsZonesOwner  = $domainDnsZonesOwner
-        ForestDnsZonesOwner  = $forestDnsZonesOwner
     }
     
+    # Application Partition can be DC=DomainDnsZones,DC=domain,DC=com or DC=ForestDnsZones,DC=domain,DC=com or maybe custom partition
+    $adForest.ApplicationPartitions | Where-Object { $_ -like '*DC=*,DC=*' } | ForEach-Object {
+        # can be DC=name,DC=domain,DC=com. W want to extract the name part
+        $applicationPartitionName = $_.Split(',')[0].Split('=')[1]
+        $fsmoRolesOwner = (Get-ADObject -Identity "CN=Infrastructure,$($_)" -Properties fsmoRoleOwner).fsmoRoleOwner
+        
+        if ($ConvertNTDSToHostName.IsPresent) {
+            $fsmoRolesOwner = (Get-ADDomainController -Filter { NTDSSettingsObjectDN -eq $fsmoRolesOwner }).HostName
+        }
+
+        $object | Add-Member -MemberType NoteProperty -Name $applicationPartitionName -Value $fsmoRolesOwner
+    }
+
     return $object
 }
