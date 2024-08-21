0cd153ecff17049911ab129f7cd122770d4c90f4
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2024-08-21 14:51:39 +0200

    Modify Get-MasterOperationRoles.ps1 to add ConvertNTDSToHostName parameter

--- Get-MasterOperationRoles.ps1
+++ Get-MasterOperationRoles.ps1
@@ -1,17 +1,28 @@
-Function Get-MasterOperationRoles {
+function Get-MasterOperationRoles {
+    param(
+        [Parameter(Mandatory = $false)]
+        [switch]$ConvertNTDSToHostName
+    )
     $adForest = Get-ADForest
     $adDomain = Get-ADDomain
 
+    $domainDnsZonesOwner = (Get-ADObject "CN=Infrastructure,DC=DomainDnsZones,$($adDomain.DistinguishedName)" -Properties fsmoRoleOwner).fsmoRoleOwner
+    $forestDnsZonesOwner = (Get-ADObject "CN=Infrastructure,DC=ForestDnsZones,$($adDomain.DistinguishedName)" -Properties fsmoRoleOwner).fsmoRoleOwner
+
+    if ($ConvertNTDSToHostName.IsPresent) {
+        $domainDnsZonesOwner = (Get-ADDomainController -Filter { NTDSSettingsObjectDN -eq $domainDnsZonesOwner }).HostName
+        $forestDnsZonesOwner = (Get-ADDomainController -Filter { NTDSSettingsObjectDN -eq $forestDnsZonesOwner }).HostName
+    }
+    
     $object = [PSCustomObject][ordered] @{
         DomainNamingMaster   = $adForest.DomainNamingMaster
         SchemaMaster         = $adForest.SchemaMaster
         InfrastructureMaster = $adDomain.InfrastructureMaster
         RIDMaster            = $adDomain.RIDMaster
         PDCEmulator          = $adDomain.PDCEmulator
-        DomainDnsZonesOwner  = (Get-ADObject "CN=Infrastructure,DC=DomainDnsZones,$($adDomain.DistinguishedName)" -Properties fsmoRoleOwner).fsmoRoleOwner
-        ForestDnsZonesOwner  = (Get-ADObject "CN=Infrastructure,DC=ForestDnsZones,$($adDomain.DistinguishedName)" -Properties fsmoRoleOwner).fsmoRoleOwner
+        DomainDnsZonesOwner  = $domainDnsZonesOwner
+        ForestDnsZonesOwner  = $forestDnsZonesOwner
     }
     
     return $object
-
 }
