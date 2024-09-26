ccd1b2bc86bdfbfc7e7689812902880e845d8018
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2024-09-26 09:58:12 +0200

    Change Get-LocalGroupMembersWithWinNT.ps1 comments

--- LocalUsersAndLocalGroups/Get-LocalGroupMembersWithWinNT.ps1
+++ LocalUsersAndLocalGroups/Get-LocalGroupMembersWithWinNT.ps1
@@ -1,6 +1,7 @@
 # Custom function instead of Get-LocalGroupMember because it's not working if member has unresolved SID
-# Get-LocalGroupMember PowerShell command doesn’t work on an AzureAD joined device as there are two unresolved SIDs in the member list. It will throw the following error.
-# Get-LocalGroupMember – Failed to compare two elements in the array. · Issue #2996 · PowerShell/PowerShell · GitHub
+# Get-LocalGroupMember PowerShell command doesn’t work on an Microosft Entra ID joined device as there are two unresolved SIDs in the member list. 
+# It will throw the following error: Failed to compare two elements in the array.
+# Issue #2996 · PowerShell/PowerShell · GitHub
 function Get-LocalGroupMembersWithWinNT {
     Param(
         [Parameter(Mandatory = $True, Position = 1)]
