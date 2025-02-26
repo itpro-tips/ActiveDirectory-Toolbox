468601f7dc9ce8653f33748c9785a887bc876a50
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-02-26 16:08:48 +0100

    Add additional user properties to Get-LocalGroupMembersWithWinNT function for enhanced member details

--- LocalUsersAndLocalGroups/Get-LocalGroupMembersWithWinNT.ps1
+++ LocalUsersAndLocalGroups/Get-LocalGroupMembersWithWinNT.ps1
@@ -43,21 +43,67 @@ function Get-LocalGroupMembersWithWinNT {
         $memberType = $_.GetType().InvokeMember('Class', 'GetProperty', $null, $_, $null)
         $memberName = $_.GetType().InvokeMember('Name', 'GetProperty', $null, $_, $null)
         $memberSID = New-Object -TypeName System.Security.Principal.SecurityIdentifier -ArgumentList $_.GetType().InvokeMember('ObjectSID', 'GetProperty', $null, $_, $null), 0
+        # lastlogin for the member
 
         if ($memberType -eq 'User' -and $principalSource -eq 'Local') {
             # return $true if the account is disabled
+            $lastLogin = $_.GetType().InvokeMember('LastLogin', 'GetProperty', $null, $_, $null)
             $memberStatus = -not ($_.GetType().InvokeMember('AccountDisabled', 'GetProperty', $null, $_, $null))
+            $lockoutObservationInterval = $_.GetType().InvokeMember('LockoutObservationInterval', 'GetProperty', $null, $_, $null)
+            $loginHours = $_.GetType().InvokeMember('LoginHours', 'GetProperty', $null, $_, $null)
+            $loginScript = $_.GetType().InvokeMember('LoginScript', 'GetProperty', $null, $_, $null)
+            $maxBadPasswordsAllowed = $_.GetType().InvokeMember('MaxBadPasswordsAllowed', 'GetProperty', $null, $_, $null)
+            $maxPasswordAge = $_.GetType().InvokeMember('MaxPasswordAge', 'GetProperty', $null, $_, $null)
+            $maxStorage = $_.GetType().InvokeMember('MaxStorage', 'GetProperty', $null, $_, $null)
+            $minPasswordAge = $_.GetType().InvokeMember('MinPasswordAge', 'GetProperty', $null, $_, $null)
+            $minPasswordLength = $_.GetType().InvokeMember('MinPasswordLength', 'GetProperty', $null, $_, $null)
+            $passwordAge = $_.GetType().InvokeMember('PasswordAge', 'GetProperty', $null, $_, $null)
+            $passwordExpired = $_.GetType().InvokeMember('PasswordExpired', 'GetProperty', $null, $_, $null)
+            $passwordHistoryLength = $_.GetType().InvokeMember('PasswordHistoryLength', 'GetProperty', $null, $_, $null)
+            $primaryGroupID = $_.GetType().InvokeMember('PrimaryGroupID', 'GetProperty', $null, $_, $null)
+            $userFlags = $_.GetType().InvokeMember('UserFlags', 'GetProperty', $null, $_, $null)
+        }
+        else {
+            $lastLogin = 'UnknownOrNotApplicable'
+            $memberStatus = 'UnknownOrNotApplicable'
+            $lockoutObservationInterval = 'UnknownOrNotApplicable'
+            $loginHours = 'UnknownOrNotApplicable'
+            $loginScript = 'UnknownOrNotApplicable'
+            $maxBadPasswordsAllowed = 'UnknownOrNotApplicable'
+            $maxPasswordAge = 'UnknownOrNotApplicable'
+            $maxStorage = 'UnknownOrNotApplicable'
+            $minPasswordAge = 'UnknownOrNotApplicable'
+            $minPasswordLength = 'UnknownOrNotApplicable'
+            $passwordAge = 'UnknownOrNotApplicable'
+            $passwordExpired = 'UnknownOrNotApplicable'
+            $passwordHistoryLength = 'UnknownOrNotApplicable'
+            $primaryGroupID = 'UnknownOrNotApplicable'
+            $userFlags = 'UnknownOrNotApplicable'
         }
         
         $object = [PSCustomObject][ordered]@{
-            Computername          = $Computer
-            GroupName             = $groupName
-            MemberName            = $memberName
-            MemberEnabled         = $memberStatus
-            MemberPath            = $path
-            MemberSid             = $memberSID
-            MemberType            = $memberType
-            MemberPrincipalSource = $principalSource
+            Computername               = $Computer
+            GroupName                  = $groupName
+            MemberName                 = $memberName
+            MemberEnabled              = $memberStatus
+            MemberPath                 = $path
+            MemberSid                  = $memberSID
+            MemberType                 = $memberType
+            MemberPrincipalSource      = $principalSource
+            LastLogin                  = $lastLogin
+            LockoutObservationInterval = $lockoutObservationInterval
+            LoginHours                 = $loginHours
+            LoginScript                = $loginScript
+            MaxBadPasswordsAllowed     = $maxBadPasswordsAllowed
+            MaxPasswordAge             = $maxPasswordAge
+            MaxStorage                 = $maxStorage
+            MinPasswordAge             = $minPasswordAge
+            MinPasswordLength          = $minPasswordLength
+            PasswordAge                = $passwordAge
+            PasswordExpired            = $passwordExpired
+            PasswordHistoryLength      = $passwordHistoryLength
+            PrimaryGroupID             = $primaryGroupID
+            UserFlags                  = $userFlags
         }
 
         $groupMembersArray.Add($object)
